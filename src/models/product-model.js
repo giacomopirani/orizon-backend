9236b19830e2eaf3baa6d296b5ffc8ea5f0f6bc7
Giacomo Pirani <giacomopirani1996@gmail.com>
2025-02-22 13:04:20 +0100

    add product model

---
+++ src/models/product-model.js
@@ -0,0 +1,27 @@
+const pool = require("../db");
+
+module.exports = {
+  async createProduct(name) {
+    const query = "INSERT INTO products (name) VALUES (?)";
+    const [result] = await pool.execute(query, [name]);
+    return result.insertId;
+  },
+
+  async getProductById(id) {
+    const query = "SELECT * FROM products WHERE id = ?";
+    const [rows] = await pool.execute(query, [id]);
+    return rows[0];
+  },
+
+  async updateProduct(id, name) {
+    const query = "UPDATE products SET name = ? WHERE id = ?";
+    const [result] = await pool.execute(query, [name, id]);
+    return result.affectedRows;
+  },
+
+  async deleteProduct(id) {
+    const query = "DELETE FROM products WHERE id = ?";
+    const [result] = await pool.execute(query, [id]);
+    return result.affectedRows;
+  },
+};
