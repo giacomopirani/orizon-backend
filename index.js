d9642942049f1c107849f8da258d6f30e281cfb7
Giacomo Pirani <giacomopirani1996@gmail.com>
2025-02-25 12:53:24 +0100

    fix last details

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const app = require("./src/app");
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server in esecuzione sulla porta ${PORT}`);
+  console.log(`Server running on the port ${PORT}`);
 });
