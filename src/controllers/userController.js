eb12f5437443e2e052c40676fef8097e3b8721d3
Giacomo Pirani <giacomopirani1996@gmail.com>
2025-02-28 13:10:42 +0100

    add delete/update user in controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -17,12 +17,44 @@ exports.createUser = async (req, res, next) => {
   }
 };
 
-exports.updateUser = (req, res) => {
-  res.json({ message: "User up to date" });
+exports.updateUser = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { nome, cognome, email } = req.body;
+
+    // Check that all required fields are present
+    if (!nome || !cognome || !email) {
+      return res
+        .status(400)
+        .json({ error: "Nome, cognome e email sono obbligatori" });
+    }
+
+    const affectedRows = await userModel.updateUser(id, {
+      nome,
+      cognome,
+      email,
+    });
+    if (affectedRows === 0) {
+      return res.status(404).json({ error: "Utente non trovato" });
+    }
+
+    res.status(200).json({ message: "Utente aggiornato correttamente" });
+  } catch (error) {
+    next(error);
+  }
 };
 
-exports.deleteUser = (req, res) => {
-  res.json({ message: "User deleted" });
+exports.deleteUser = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const affectedRows = await userModel.deleteUser(id);
+    if (affectedRows === 0) {
+      return res.status(404).json({ error: "Utente non trovato" });
+    }
+    res.status(200).json({ message: "Utente eliminato correttamente" });
+  } catch (error) {
+    next(error);
+  }
 };
 
 exports.getAllUsers = async (req, res, next) => {
